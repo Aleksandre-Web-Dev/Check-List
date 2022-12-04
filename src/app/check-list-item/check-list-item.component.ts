d57f73e593550618e2dc24e3a19b867f1670e005
Aleksandre-Web-Dev <pitskhelauri.aleksandre@gmail.com>
2022-12-04 23:14:34 +0400

    Clear All Tasks, Task Counters

--- src/app/check-list-item/check-list-item.component.ts
+++ src/app/check-list-item/check-list-item.component.ts
@@ -8,12 +8,19 @@ import { ITask } from '../add-form/add-form.component';
 })
 export class CheckListItemComponent {
   @Input() checkList: Array<ITask> = [];
+  completeCounter: number = 0;
+
   removeTask(index: number) {
     this.checkList.splice(index, 1);
   }
 
+  clearAll() {
+    this.checkList.splice(0);
+  }
+
   markAsComplete(index: number) {
     this.checkList[index].isComplete = true;
-    this.checkList.splice(index, 1);
+    this.checkList.sort((a, b) => Number(a.isComplete) - Number(b.isComplete));
+    this.completeCounter++;
   }
 }
